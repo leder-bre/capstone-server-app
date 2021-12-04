10f5c46a74c4bf17d4888dfddd5169dede009e55
Brendan Leder <brendan@oodler.ca>
2021-12-03 19:20:00 -0500

    Added basic graph that reads from CSV

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # capstone-server-app
+
+Server application for ECE496 capstone project.
+To set up dependencies run `npm install` after pulling project.
+Run with `DEBUG=capstone_app:* npm start`
+
+## TODO:
+
+Make d3 run from local install instead of downloading script
+Make our own d3.css since this one's from link below
+
+## Other info
+
+Used http://bl.ocks.org/d3netxer/10a28b7aee406f4e7fce for d3 stuff
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const csv = require('csv-parser')
+const fs = require('fs')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // const results = [];
+
+  // fs.createReadStream('./public/DB_V2.csv')
+  //   .pipe(csv())
+  //   .on('data', (data) => results.push(data))
+  //   .on('end', () => {
+  //     console.log('csv parsed');
+  //     var timestamps = [];
+  //     var values = [];
+  //     for (row in results) {
+  //       timestamps.push(results[row]['TIME']);
+  //       values.push(results[row]['VALUE']);
+  //     }
+  //     console.log(timestamps);
+  //     console.log(values);
+      
+  //   });
+    res.render('index', {title: 'Express', timestamps, values});
 });
 
 module.exports = router;
