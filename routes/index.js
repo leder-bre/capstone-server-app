8feeea9fb74454ec3cb9c51d32aca1ef7196b181
Brendan Leder <brendan@oodler.ca>
2021-12-03 19:36:48 -0500

    fixed issue with timestamps, got axis to scale properly

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
   //     console.log(values);
       
   //   });
-    res.render('index', {title: 'Express', timestamps, values});
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
