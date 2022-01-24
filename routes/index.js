02069340002470c261462cedab555be09ff1303f
Brendan Leder <brendan@oodler.ca>
2022-01-24 14:02:03 -0500

    Added request to google sheets and .env file to store credentials

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv').config();
+
 const csv = require('csv-parser')
-const fs = require('fs')
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
+const { request } = require('http');
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const sheets = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
+  var sheet_output = null;
+  sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: 'A1:D100',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    sheet_output = res.data.values;
+    if (sheet_output.length) {
+      console.log('Name, Major:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      sheet_output.map((row) => {
+        console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
+
+  
   // const results = [];
 
   // fs.createReadStream('./public/DB_V2.csv')
@@ -23,7 +50,7 @@ router.get('/', function(req, res, next) {
   //     console.log(values);
       
   //   });
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Water Level Monitor' });
 });
 
 module.exports = router;
