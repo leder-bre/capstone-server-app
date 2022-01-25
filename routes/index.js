ebdc0989b512999daa6be4c7d751041cceb8fc8e
Brendan Leder <brendan@oodler.ca>
2022-01-24 20:03:08 -0500

    Added plotly graph that reads from Google Sheets each refresh

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,14 @@ const { request } = require('http');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, router_res, next) {
   const sheets = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
   var sheet_output = null;
+  var graph_x = [];
+  var graph_y = [];
   sheets.spreadsheets.values.get({
     spreadsheetId: process.env.GOOGLE_SHEET_ID,
-    range: 'A1:D100',
+    range: 'A2:D100',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     sheet_output = res.data.values;
@@ -26,31 +28,14 @@ router.get('/', function(req, res, next) {
       // Print columns A and E, which correspond to indices 0 and 4.
       sheet_output.map((row) => {
         console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`);
+        graph_x.push(row[1]);
+        graph_y.push(row[3]);
       });
+      router_res.render('index', { title: 'Water Level Monitor', graph_x: graph_x.toString(), graph_y: graph_y.toString() });
     } else {
       console.log('No data found.');
     }
   });
-
-  
-  // const results = [];
-
-  // fs.createReadStream('./public/DB_V2.csv')
-  //   .pipe(csv())
-  //   .on('data', (data) => results.push(data))
-  //   .on('end', () => {
-  //     console.log('csv parsed');
-  //     var timestamps = [];
-  //     var values = [];
-  //     for (row in results) {
-  //       timestamps.push(results[row]['TIME']);
-  //       values.push(results[row]['VALUE']);
-  //     }
-  //     console.log(timestamps);
-  //     console.log(values);
-      
-  //   });
-    res.render('index', { title: 'Water Level Monitor' });
 });
 
 module.exports = router;
