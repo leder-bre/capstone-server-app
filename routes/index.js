130d38a2e0e9483c29d77f1e105199a4364181b1
Brendan Leder <brendan@oodler.ca>
2022-01-24 20:42:07 -0500

    Now gets full column from Google Sheets, and plotly properly formats as date-time

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
 const { request } = require('http');
+const moment = require('moment');
+
+const currentFormatString = "MM/DD/YYYY hh:mm:ss";
+const newFormatString     = "YYYY-MM-DD hh:mm:ss";
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
@@ -19,16 +23,16 @@ router.get('/', function(req, router_res, next) {
   var graph_y = [];
   sheets.spreadsheets.values.get({
     spreadsheetId: process.env.GOOGLE_SHEET_ID,
-    range: 'A2:D100',
+    range: 'A2:D',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     sheet_output = res.data.values;
     if (sheet_output.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
+      // Print columns A to D, and map corresponding output into array
       sheet_output.map((row) => {
-        console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`);
-        graph_x.push(row[1]);
+        // console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`); // uncomment to see GSheets info printed out
+        var formattedDate = moment(row[1], currentFormatString).format(newFormatString); // format from GSheets format to Plotly format
+        graph_x.push(formattedDate);
         graph_y.push(row[3]);
       });
       router_res.render('index', { title: 'Water Level Monitor', graph_x: graph_x.toString(), graph_y: graph_y.toString() });
