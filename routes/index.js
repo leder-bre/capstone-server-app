7ba625bb4e62fc1663a4466b379ed3d55bb38c77
Brendan Leder <brendan@oodler.ca>
2022-04-12 15:58:14 -0400

    Added favicon, multiple graphs

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,12 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 router.get('/', function(req, router_res, next) {
   const sheets = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
   var sheet_output = null;
-  var graph_x = [];
-  var graph_y = [];
+  var graph1_x = [];
+  var graph1_y = [];
+  var graph2_x = [];
+  var graph2_y = [];
+  var graph3_x = [];
+  var graph3_y = [];
   sheets.spreadsheets.values.get({
     spreadsheetId: process.env.GOOGLE_SHEET_ID,
     range: 'A2:D',
@@ -32,10 +36,22 @@ router.get('/', function(req, router_res, next) {
       sheet_output.map((row) => {
         // console.log(`${row[0]}, ${row[1]}, ${row[2]}, ${row[3]}`); // uncomment to see GSheets info printed out
         var formattedDate = moment(row[1], currentFormatString).format(newFormatString); // format from GSheets format to Plotly format
-        graph_x.push(formattedDate);
-        graph_y.push(row[3]);
+        console.log("Row2:" + row[2].toString())
+        if (row[2].toString() == '1') {
+          console.log('1 ran')
+          graph1_x.push(formattedDate);
+          graph1_y.push(row[3]);
+        } else if (row[2].toString() == '2') {
+          graph2_x.push(formattedDate);
+          graph2_y.push(row[3]);
+        } else if (row[2].toString() == '3') {
+          graph3_x.push(formattedDate);
+          graph3_y.push(row[3]);
+        } else {
+          console.log("Invalid row in spreadsheet")
+        }
       });
-      router_res.render('index', { title: 'Water Level Monitor', graph_x: graph_x.toString(), graph_y: graph_y.toString() });
+      router_res.render('index', { title: 'Water Level Monitor', graph1_x: graph1_x.toString(), graph1_y: graph1_y.toString(), graph2_x: graph2_x.toString(), graph2_y: graph2_y.toString(), graph3_x: graph3_x.toString(), graph3_y: graph3_y.toString() });
     } else {
       console.log('No data found.');
     }
